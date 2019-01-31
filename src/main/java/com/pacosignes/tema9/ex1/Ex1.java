e570b3aa1feef9a4b881cea0bf272a42a49dc643
Paco <pacos16@gmail.com>
2019-01-31 19:00:11 +0100

    nanit

--- src/main/java/com/pacosignes/tema9/ex1/Ex1.java
+++ src/main/java/com/pacosignes/tema9/ex1/Ex1.java
@@ -1,4 +1,32 @@
 package com.pacosignes.tema9.ex1;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Ex1 {
+
+    public static void execute() {
+        Scanner lector = new Scanner(System.in);
+        int num=0;
+        boolean correcto=true;
+
+        do{
+
+            System.out.println("Dime un numero");
+            try {
+                num = lector.nextInt();
+                lector.nextLine();
+            } catch (InputMismatchException e) {
+
+                System.out.println("Mismatch Excepcion");
+                correcto=false;
+            }
+            if(!correcto && num>=0){
+                System.out.println("fatal");
+            }
+
+        }while(correcto && num>=0);
+
+    }
+
+
 }
